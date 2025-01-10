e218f410b670ca1e1bb04ba8e0e453f5949bfef8
moheb <134a137z@gmail.com>
2025-01-10 07:01:40 +0330

    finish the search of tour page and add inline svg from Popular tours and add js for it

--- js/app.js
+++ js/app.js
@@ -51,3 +51,39 @@ let mQuestionsImg5 = document.getElementById("mQuestionsImg5");
     mQuestionsImg5.classList.toggle("rotate");
 })
 
+let star5= document.getElementById("star5")
+let star4= document.getElementById("star4")
+let star3= document.getElementById("star3")
+let star2= document.getElementById("star2")
+let star1= document.getElementById("star1")
+
+star5.addEventListener("click",()=>{
+    star1.classList.toggle("mStar")
+    star2.classList.toggle("mStar")
+    star3.classList.toggle("mStar")
+    star4.classList.toggle("mStar")
+    star5.classList.toggle("mStar")
+})
+
+star4.addEventListener("click",()=>{
+    star1.classList.toggle("mStar")
+    star2.classList.toggle("mStar")
+    star3.classList.toggle("mStar")
+    star4.classList.toggle("mStar")
+})
+
+star3.addEventListener("click",()=>{
+    star1.classList.toggle("mStar")
+    star2.classList.toggle("mStar")
+    star3.classList.toggle("mStar")
+})
+
+star2.addEventListener("click",()=>{
+    star1.classList.toggle("mStar")
+    star2.classList.toggle("mStar")
+})
+
+star1.addEventListener("click",()=>{
+    star1.classList.toggle("mStar")
+})
+
